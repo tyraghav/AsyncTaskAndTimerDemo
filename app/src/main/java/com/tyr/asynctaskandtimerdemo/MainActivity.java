cab381319c751ad31857af341b4e20d29bbf491a
TYR <tyraghav@outlook.com>
2018-11-21 04:47:44 +0530

    App Ready
    All planned Tasks Done

--- app/src/main/java/com/tyr/asynctaskandtimerdemo/MainActivity.java
+++ app/src/main/java/com/tyr/asynctaskandtimerdemo/MainActivity.java
@@ -1,19 +1,33 @@
 package com.tyr.asynctaskandtimerdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
-import java.util.Timer;
+import java.util.concurrent.ExecutionException;
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +42,18 @@ public class MainActivity extends AppCompatActivity {
     Button optionButton6;
     SeekBar timerSetSeekBar;
     ListView loggerListView;
+    EditText cityNames;
     int[] buttonColors = new int[6];
-    int maxTime = 0;
-    Random randomSix;
+    int maxTime;
+    int firstNum = 0;
+    int MagicNumber = 6;
+    Random rn = new Random(6);
     boolean isCounterActive = false;
+    Bitmap Success;
+    Bitmap Failure;
+    Bitmap OnLoad;
+    Bitmap Bomb;
+    CountDownTimer gameTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +76,12 @@ public class MainActivity extends AppCompatActivity {
         buttonColors[5]=R.color.buttonColor6;
         timerSetSeekBar = (SeekBar)findViewById(R.id.timerSetSeekBar);
         loggerListView = (ListView)findViewById(R.id.loggerListView);
+        cityNames = (EditText) findViewById(R.id.cityNames);
         optionsButtonViewSetter(1);
+        maxTime = 30;
         timerTextViewSetter(maxTime);
         timerSetSeekBar.setMax(600);
         timerSetSeekBar.setProgress(30);
-        maxTime = 30;
         timerSetSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -76,6 +99,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        loggerListView = (ListView)findViewById(R.id.loggerListView);
+        loggerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String CitySelected = loggerListView.getItemAtPosition(i).toString();
+                Log.i("Check City", "AAAA"+CitySelected);
+                ClimateUpdates climate = new ClimateUpdates();
+                climate.execute(new String[]{CitySelected, Integer.toString(firstNum)});
+            }
+        });
+        loggerListView.setVisibility(View.INVISIBLE);
+        getImage("ONLOAD");
         /*final Handler handler = new Handler();
         Runnable run=new Runnable() {
             int i=0;
@@ -88,32 +123,108 @@ public class MainActivity extends AppCompatActivity {
         };
         run.run();*/
     }
-
-    void startButtonClicked(View view){
-        startButton.setVisibility(View.INVISIBLE);
-        String presentTime = (String)timerTextView.getText();
-
-        CountDownTimer gameTimer = new CountDownTimer(maxTime*1000+70,1000) {
-            int firstNum = 0;
-
-            @Override
-            public void onTick(long milliSeconds) {
-                //Toast.makeText(getApplicationContext(), Long.toString(oneSecond), Toast.LENGTH_SHORT).show();
-                timerTextViewSetter(firstNum);
-                if(firstNum!=0 && firstNum<=maxTime-5){
-                    optionsButtonViewSetter(firstNum);
-                }
-                firstNum++;
+    void getImage(String TypeImage){
+        try {
+        if(TypeImage.equalsIgnoreCase("SUCCESS")){
+            if(Success==null){
+                ImageDownloader imageService = new ImageDownloader();
+                Log.i("IN GET IMAGE","SUCCESS");
+                Success = imageService.execute("https://chehockey.files.wordpress.com/2014/12/winner.jpg?w=645&h=390").get();
+                changeImageView.setImageBitmap(Success);
             }
-
-            @Override
-            public void onFinish() {
-                isCounterActive = false;
-                timerTextView.setText("00 : 00");
+        } else if(TypeImage.equalsIgnoreCase("FAILURE")){
+            if(Failure==null){
+                ImageDownloader imageService = new ImageDownloader();
+                Log.i("IN GET IMAGE","FAILURE");
+                Failure = imageService.execute("https://tamil.oneindia.com/img/2016/03/08-1457414709-tamilisai-soundararajan46-600.jpg").get();
+                changeImageView.setImageBitmap(Failure);
             }
+        } else if(TypeImage.equalsIgnoreCase("BOMB")){
+            if(Bomb==null){
+                ImageDownloader imageService = new ImageDownloader();
+                Log.i("IN GET IMAGE","BOMB");
+                Bomb = imageService.execute("https://mundabor.files.wordpress.com/2014/04/bomb.jpg").get();
+                changeImageView.setImageBitmap(Bomb);
+            }
+        } else {
+            if(OnLoad==null){
+                ImageDownloader imageService = new ImageDownloader();
+                Log.i("IN GET IMAGE","ONLOAD");
+                OnLoad = imageService.execute("https://www.thehindu.com/news/national/tamil-nadu/tbhjf5/article24962647.ece/alternates/FREE_660/TH17HRAJARAMANATHAPURAM").get();
+                changeImageView.setImageBitmap(OnLoad);
+            }
+        }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+    void stopTimer(int presentTime){
+        if(presentTime==0) {
+            getImage("FAILURE");
+            gameTimer.cancel();
+        }else{
+            getImage("SUCCESS");
+            gameTimer.cancel();;
+
+        }
+            startButton.setVisibility(View.VISIBLE);
+            timerSetSeekBar.setVisibility(View.VISIBLE);
+            loggerListView.setVisibility(View.INVISIBLE);
+            cityNames.setVisibility(View.VISIBLE);
+            isCounterActive = false;
+            timerTextView.setText("00 : 00");
+            maxTime = 0;
+            timerSetSeekBar.setProgress(0);
+    }
+
+    void onOptionClicked(View view){
+        Button OptionButton = (Button)view;
+        Log.i("IN CLICK",Integer.toString(MagicNumber));
+        if(Integer.toString(MagicNumber).equalsIgnoreCase(OptionButton.getTag().toString())){
+            stopTimer(1);
+            Toast.makeText(getApplicationContext(), "SUCCESS", Toast.LENGTH_SHORT).show();
         };
-        gameTimer.start();
-        isCounterActive = true;
+    }
+    void startButtonClicked(View view){
+        if(maxTime!=0) {
+            getImage("BOMB");
+            startButton.setVisibility(View.INVISIBLE);
+            timerSetSeekBar.setVisibility(View.INVISIBLE);
+            String presentTime = (String) timerTextView.getText();
+            ClimateUpdates climate;
+            String cNames = cityNames.getText().toString();
+            cityNames.setVisibility(View.INVISIBLE);
+            loggerListView.setVisibility(View.VISIBLE);
+            ArrayList<String> cNm = new ArrayList<String>(Arrays.asList(cNames.split(",")));
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cNm);
+            loggerListView.setAdapter(aa);
+            gameTimer = new CountDownTimer(maxTime * 1000 + 70, 1000) {
+
+                @Override
+                public void onTick(long milliSeconds) {
+                    MagicNumber=rn.nextInt(6);
+                    timerTextViewSetter(firstNum);
+                    if (firstNum != 0 && firstNum <= maxTime - 5) {
+                        optionsButtonViewSetter(firstNum);
+                    }
+                    firstNum++;
+
+                }
+
+                @Override
+                public void onFinish() {
+                    Toast.makeText(getApplicationContext(),"FAILURE", Toast.LENGTH_SHORT).show();
+                    stopTimer(0);
+                }
+            };
+            gameTimer.start();
+            isCounterActive = true;
+        }else{
+            isCounterActive = false;
+            Toast.makeText(getApplicationContext(), "Please select valid number", Toast.LENGTH_SHORT).show();
+        }
     }
     void optionsButtonViewSetter(int firstNum){
         optionButton1.setText(Integer.toString(firstNum));
@@ -146,4 +257,78 @@ public class MainActivity extends AppCompatActivity {
         }
         timerTextView.setText(minute+" : "+second);
     }
+
+    class ImageDownloader extends  AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                InputStream is;
+                if(strings[0].substring(0,5).equalsIgnoreCase("https")){
+                    HttpsURLConnection con=(HttpsURLConnection)url.openConnection();
+                    con.connect();
+                    is=con.getInputStream();
+                    Log.i("HII","HTTPS");
+
+                } else {
+                    HttpURLConnection con=(HttpURLConnection)url.openConnection();
+                    con.connect();
+                    is=con.getInputStream();
+                    Log.i("HII","HTTP");
+                }
+                Bitmap myBitmMap = BitmapFactory.decodeStream(is);
+                if(myBitmMap==null){
+                    Log.i("HII","NULL");
+                }
+                return myBitmMap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    class ClimateUpdates extends AsyncTask<String , Void , String >{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String Location = strings[0];
+                URL url = new URL( "http://api.openweathermap.org/data/2.5/weather?q="+Location+"&APPID=0a18c94e863e332e5cbcf4abec48fb66");
+                HttpURLConnection con=(HttpURLConnection)url.openConnection();
+                InputStream is=con.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                String Result="";
+                int data = 0;
+                while(data!=-1){
+                    data = isr.read();
+                    char curr = (char)data;
+                    Result+=curr;
+                }
+                JSONObject jObj = new JSONObject(Result);
+                JSONArray Weather = (JSONArray)jObj.get("weather");
+                JSONObject main = (JSONObject)jObj.get("main");
+                Double temp = (Double)main.get("temp");
+                JSONObject lWeather = Weather.getJSONObject(0);
+                Result = new String(strings[1]+" : "+ Location +'\n' + lWeather.getString("main") + " : " + lWeather.getString("description") + "\n" + "Temperature : " + temp.toString() + 'F');
+                return Result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s!=null) {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "ERROR OCCURED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
